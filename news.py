65be768d6dff3aa878d24c11440dfbe18b2ac11d
DeathGXD <gxdongever@163.com>
2018-12-10 17:46:33 +0800

    update

--- news.py
+++ news.py
@@ -12,24 +12,28 @@ import demjson
 
 class Handler(BaseHandler):
     crawl_config = {
+        'header': {
+            'Accept-Charset': 'UTF-8',
+            'Accept': '*/*',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Connection': 'keep-alive',
+            'Cookie': 'vjuids=-7a4563b9.1653556d662.0.7fdee0f2bc22a; _ntes_nnid=7d9dc0dec6548bacf9960702c1bc9ee0,1534198208121; _ntes_nuid=7d9dc0dec6548bacf9960702c1bc9ee0; __gads=ID=f08dbcff9cc85b40:T=1534144217:S=ALNI_MZAduzhcd2yULxO8B8ol10pZrELXA; UM_distinctid=165d184bfe654e-0c86f82c2ba54-61547428-100200-165d184bfe722e; usertrack=ezq0o1vD4RccepHSl7ADAg==; hb_MA-A924-182E1997E62F_source=172.16.25.72%3A5000; mp_MA-A924-182E1997E62F_hubble=%7B%22sessionReferrer%22%3A%20%22http%3A%2F%2Fbj.house.163.com%2F%3Fqstr%3D%22%2C%22updatedTime%22%3A%201541579766562%2C%22sessionStartTime%22%3A%201541579766552%2C%22deviceUdid%22%3A%20%2280ee3ba7-0103-425e-94af-176f50ea570a%22%2C%22persistedTime%22%3A%201541579766544%2C%22LASTEVENT%22%3A%20%7B%22eventId%22%3A%20%22househome%22%2C%22time%22%3A%201541579766562%7D%2C%22sessionUuid%22%3A%20%228fa2e272-060a-4d08-8779-7b4bc52b0c7f%22%7D; __f_=1541581502682; mail_psc_fingerprint=d80b3cb4cb348c84c998dc9b6997bdbd; Province=021; City=021; _antanalysis_s_id=1542695162868; hb_MA-BFF5-63705950A31C_source=172.16.25.72%3A5000; vjlast=1534198208.1543386830.11; NNSSPID=cd86c3ac37c54d049b53f637fc224bff; ne_analysis_trace_id=1543393250323; pgr_n_f_l_n3=29aa9bad2ea04a5015433931967807731; s_n_f_l_n3=29aa9bad2ea04a501543396871923; vinfo_n_f_l_n3=29aa9bad2ea04a50.1.23.1534198208162.1543393656200.1543396872633; mp_MA-B4F0-3EDB3213C01D_hubble=%7B%22sessionReferrer%22%3A%20%22%22%2C%22updatedTime%22%3A%201543454550181%2C%22sessionStartTime%22%3A%201543454461691%2C%22deviceUdid%22%3A%20%225c5032a1-3284-48d1-b0b5-4932bd3dc818%22%2C%22initial_referrer%22%3A%20%22%24direct%22%2C%22initial_referring_domain%22%3A%20%22%24direct%22%2C%22persistedTime%22%3A%201543454461684%2C%22LASTEVENT%22%3A%20%7B%22eventId%22%3A%20%22da_screen%22%2C%22time%22%3A%201543454550181%7D%2C%22sessionUuid%22%3A%20%22d011a078-aaee-45d7-92a2-82a7dfed3a08%22%7D',
+            'Host': '3g.163.com',
+            'Referer': 'https://3g.163.com/touch/news/',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3554.0 Mobile Safari/537.36'
+        }
     }
 
-    @every(minutes=1 * 60)
+    @every(seconds=3 * 60)
     def on_start(self):
         self.conn = pymysql.connect(host="47.101.146.57", port=2018, user="root", password="Liuku!!!111",
                                     db="dm_report", charset='utf8')
         self.conn.autocommit = True
         print("已打开数据库链接")
-        self.crawl('http://3g.163.com/news', callback=self.index_page)
+        self.crawl('https://3g.163.com/touch/news/', callback=self.index_page, connect_timeout=10, timeout=30,
+                   retries=0, auto_recrawl=False, headers=self.crawl_config["header"])
 
-    def on_finished(self):
-        if hasattr(self, 'conn'):
-            self.conn.close()
-            print("数据库链接已关闭！")
-
-        self.on_start(self)
-
-    @config(age=2 * 1 * 60 * 60)
+    @config(age=3 * 60)
     def index_page(self, response):
         news_type = '{"news_type": [{"name": "推荐", "id": "BA8J7DG9wangning"},' + \
                     '{"name": "军事", "id": "BAI67OGGwangning"},' + \
@@ -78,20 +82,9 @@ class Handler(BaseHandler):
                 self.request_data(url, data['id'], data['name'])
 
     def request_data(self, url=None, id=None, typename=None):
-        header = {
-            'Accept-Charset': 'UTF-8',
-            'Accept': '*/*',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Connection': 'keep-alive',
-            'Cookie': 'vjuids=-7a4563b9.1653556d662.0.7fdee0f2bc22a; _ntes_nnid=7d9dc0dec6548bacf9960702c1bc9ee0,1534198208121; _ntes_nuid=7d9dc0dec6548bacf9960702c1bc9ee0; __gads=ID=f08dbcff9cc85b40:T=1534144217:S=ALNI_MZAduzhcd2yULxO8B8ol10pZrELXA; UM_distinctid=165d184bfe654e-0c86f82c2ba54-61547428-100200-165d184bfe722e; usertrack=ezq0o1vD4RccepHSl7ADAg==; hb_MA-A924-182E1997E62F_source=172.16.25.72%3A5000; mp_MA-A924-182E1997E62F_hubble=%7B%22sessionReferrer%22%3A%20%22http%3A%2F%2Fbj.house.163.com%2F%3Fqstr%3D%22%2C%22updatedTime%22%3A%201541579766562%2C%22sessionStartTime%22%3A%201541579766552%2C%22deviceUdid%22%3A%20%2280ee3ba7-0103-425e-94af-176f50ea570a%22%2C%22persistedTime%22%3A%201541579766544%2C%22LASTEVENT%22%3A%20%7B%22eventId%22%3A%20%22househome%22%2C%22time%22%3A%201541579766562%7D%2C%22sessionUuid%22%3A%20%228fa2e272-060a-4d08-8779-7b4bc52b0c7f%22%7D; __f_=1541581502682; mail_psc_fingerprint=d80b3cb4cb348c84c998dc9b6997bdbd; Province=021; City=021; _antanalysis_s_id=1542695162868; hb_MA-BFF5-63705950A31C_source=172.16.25.72%3A5000; vjlast=1534198208.1543386830.11; NNSSPID=cd86c3ac37c54d049b53f637fc224bff; ne_analysis_trace_id=1543393250323; pgr_n_f_l_n3=29aa9bad2ea04a5015433931967807731; s_n_f_l_n3=29aa9bad2ea04a501543396871923; vinfo_n_f_l_n3=29aa9bad2ea04a50.1.23.1534198208162.1543393656200.1543396872633; mp_MA-B4F0-3EDB3213C01D_hubble=%7B%22sessionReferrer%22%3A%20%22%22%2C%22updatedTime%22%3A%201543454550181%2C%22sessionStartTime%22%3A%201543454461691%2C%22deviceUdid%22%3A%20%225c5032a1-3284-48d1-b0b5-4932bd3dc818%22%2C%22initial_referrer%22%3A%20%22%24direct%22%2C%22initial_referring_domain%22%3A%20%22%24direct%22%2C%22persistedTime%22%3A%201543454461684%2C%22LASTEVENT%22%3A%20%7B%22eventId%22%3A%20%22da_screen%22%2C%22time%22%3A%201543454550181%7D%2C%22sessionUuid%22%3A%20%22d011a078-aaee-45d7-92a2-82a7dfed3a08%22%7D',
-            'Host': '3g.163.com',
-            'Referer': 'https://3g.163.com/touch/news/',
-            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3554.0 Mobile Safari/537.36'
-        }
-
         data = None
 
-        rq = request.Request(url, data=data, headers=header)
+        rq = request.Request(url, data=data, headers=self.crawl_config["header"])
         res = request.urlopen(rq)
         respoen = res.read()
         result = str(respoen, encoding="utf-8")[9:-1]
@@ -133,13 +126,18 @@ class Handler(BaseHandler):
             return
 
         cursor = self.conn.cursor()
-        sql = '''insert into toutiao_news(title,news_time,url,from_source,author,news_type,digest,contents,commentCount,image_url,check_status) values ('{}','{}','{}','{}','{}','{}','{}', '{}','{}','{}','{}');'''.format(
-            result['title'].replace(chr(39), "\\'"), result['news_time'], result['url'], result['from_source'],
-            result['author'], result['news_type'], result['digest'], result['content'].replace(chr(39), "\\'"),
-            result['commentCount'], result['image_url'], '0')
-        print(sql)
-        cursor.execute(sql)
-        self.conn.commit()
+
+        row = cursor.execute('''select id from toutiao_news where url='{}';'''.format(result['url']))
+
+        if row == 0:
+            sql = '''insert into toutiao_news(title,news_time,url,from_source,author,news_type,digest,contents,commentCount,image_url,check_status) values ('{}','{}','{}','{}','{}','{}','{}', '{}','{}','{}','{}');'''.format(
+                result['title'].replace(chr(39), "\\'"), result['news_time'], result['url'], result['from_source'],
+                result['author'], result['news_type'], result['digest'], result['content'].replace(chr(39), "\\'"),
+                result['commentCount'], result['image_url'], '0')
+
+            cursor.execute(sql)
+            self.conn.commit()
+
         cursor.close()
         print("数据保存成功")
 
