287bbe88419a133ac299461ca4ceabd7ade6415c
DeathGXD <gxdongever@163.com>
2019-01-24 10:14:32 +0800

    update

---
+++ test/test2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+#!coding=utf-8
+
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from bs4 import BeautifulSoup
+import json
+
+
+def jsonParse():
+    data = "{\"abstract\":\"肝脏是人体非常重要的一个器官，肝脏能起到非常大的解毒作用。如果人体的肝脏功能不好的话，那么身体里面的一些毒素就无法排出去，久而久也会出现一些中毒的症状。\",\"action_list\":[{\"action\":1,\"desc\":\"\",\"extra\":{}},{\"action\":3,\"desc\":\"\",\"extra\":{}},{\"action\":7,\"desc\":\"\",\"extra\":{}},{\"action\":9,\"desc\":\"\",\"extra\":{}}],\"aggr_type\":1,\"allow_download\":false,\"article_sub_type\":0,\"article_type\":0,\"article_url\":\"http://toutiao.com/group/6649155791372698119/\",\"article_version\":0,\"ban_comment\":0,\"behot_time\":1548230017,\"bury_count\":0,\"cell_flag\":262155,\"cell_layout_style\":1,\"cell_type\":0,\"comment_count\":0,\"content_decoration\":\"\",\"cursor\":1548230017000,\"digg_count\":1,\"display_url\":\"http://toutiao.com/group/6649155791372698119/\",\"filter_words\":[{\"id\":\"8:0\",\"is_selected\":false,\"name\":\"看过了\"},{\"id\":\"9:1\",\"is_selected\":false,\"name\":\"内容太水\"},{\"id\":\"5:39339291\",\"is_selected\":false,\"name\":\"拉黑作者:家庭医生在线网\"},{\"id\":\"2:31135323\",\"is_selected\":false,\"name\":\"不想看:养生\"},{\"id\":\"6:16375\",\"is_selected\":false,\"name\":\"不想看:肝脏\"}],\"forward_info\":{\"forward_count\":0},\"gallary_image_count\":3,\"group_id\":6649155791372698119,\"has_image\":true,\"has_m3u8_video\":false,\"has_mp4_video\":0,\"has_video\":false,\"hot\":0,\"ignore_web_transform\":1,\"image_list\":[{\"height\":575,\"uri\":\"list/dfic-imagehandler/f9b7f730-1734-4e37-9784-ff5769eaa465\",\"url\":\"http://p1-tt.bytecdn.cn/list/300x196/dfic-imagehandler/f9b7f730-1734-4e37-9784-ff5769eaa465.webp\",\"url_list\":[{\"url\":\"http://p1-tt.bytecdn.cn/list/300x196/dfic-imagehandler/f9b7f730-1734-4e37-9784-ff5769eaa465.webp\"},{\"url\":\"http://p9-tt.bytecdn.cn/list/300x196/dfic-imagehandler/f9b7f730-1734-4e37-9784-ff5769eaa465.webp\"},{\"url\":\"http://p3-tt.bytecdn.cn/list/300x196/dfic-imagehandler/f9b7f730-1734-4e37-9784-ff5769eaa465.webp\"}],\"width\":1023},{\"height\":675,\"uri\":\"list/dfic-imagehandler/46622e05-56ff-4ed1-b657-b6b5a16daaac\",\"url\":\"http://p3-tt.bytecdn.cn/list/300x196/dfic-imagehandler/46622e05-56ff-4ed1-b657-b6b5a16daaac.webp\",\"url_list\":[{\"url\":\"http://p3-tt.bytecdn.cn/list/300x196/dfic-imagehandler/46622e05-56ff-4ed1-b657-b6b5a16daaac.webp\"},{\"url\":\"http://p9-tt.bytecdn.cn/list/300x196/dfic-imagehandler/46622e05-56ff-4ed1-b657-b6b5a16daaac.webp\"},{\"url\":\"http://p1-tt.bytecdn.cn/list/300x196/dfic-imagehandler/46622e05-56ff-4ed1-b657-b6b5a16daaac.webp\"}],\"width\":1200},{\"height\":675,\"uri\":\"list/dfic-imagehandler/07b6b1e2-a979-4af4-a681-5e5ef8c959f6\",\"url\":\"http://p9-tt.bytecdn.cn/list/300x196/dfic-imagehandler/07b6b1e2-a979-4af4-a681-5e5ef8c959f6.webp\",\"url_list\":[{\"url\":\"http://p9-tt.bytecdn.cn/list/300x196/dfic-imagehandler/07b6b1e2-a979-4af4-a681-5e5ef8c959f6.webp\"},{\"url\":\"http://p1-tt.bytecdn.cn/list/300x196/dfic-imagehandler/07b6b1e2-a979-4af4-a681-5e5ef8c959f6.webp\"},{\"url\":\"http://p9-tt.bytecdn.cn/list/300x196/dfic-imagehandler/07b6b1e2-a979-4af4-a681-5e5ef8c959f6.webp\"}],\"width\":1200}],\"interaction_data\":\"\",\"is_subject\":false,\"item_id\":6649155791372698119,\"item_version\":0,\"keywords\":\"唇色发黑,早上起床,肝脏功能不好,眼睛,人体\",\"level\":0,\"log_pb\":{\"impr_id\":\"20190123161607010023075223047865D\",\"is_following\":\"0\"},\"media_info\":{\"avatar_url\":\"http://p1.pstatp.com/large/123200176d3c39c0cfda\",\"follow\":false,\"is_star_user\":false,\"media_id\":1554938722203650,\"name\":\"家庭医生在线网\",\"recommend_reason\":\"\",\"recommend_type\":0,\"user_id\":53324103362,\"user_verified\":true,\"verified_content\":\"\"},\"media_name\":\"家庭医生在线网\",\"middle_image\":{\"height\":575,\"uri\":\"list/dfic-imagehandler/f9b7f730-1734-4e37-9784-ff5769eaa465\",\"url\":\"http://p1-tt.bytecdn.cn/list/300x196/dfic-imagehandler/f9b7f730-1734-4e37-9784-ff5769eaa465.webp\",\"url_list\":[{\"url\":\"http://p1-tt.bytecdn.cn/list/300x196/dfic-imagehandler/f9b7f730-1734-4e37-9784-ff5769eaa465.webp\"},{\"url\":\"http://p3-tt.bytecdn.cn/list/300x196/dfic-imagehandler/f9b7f730-1734-4e37-9784-ff5769eaa465.webp\"},{\"url\":\"http://p9-tt.bytecdn.cn/list/300x196/dfic-imagehandler/f9b7f730-1734-4e37-9784-ff5769eaa465.webp\"}],\"width\":1023},\"need_client_impr_recycle\":1,\"publish_time\":1548129107,\"read_count\":531,\"repin_count\":2,\"rid\":\"20190123161607010023075223047865D\",\"share_count\":0,\"share_info\":{\"cover_image\":null,\"description\":null,\"on_suppress\":0,\"share_type\":{\"pyq\":0,\"qq\":0,\"qzone\":0,\"wx\":0},\"share_url\":\"http://m.toutiao.com/a6649155791372698119/?iid=0\\u0026app=news_article\\u0026is_hit_share_recommend=0\",\"title\":\"晨起有这3个表现，是肝脏在向你报告：我生病了\",\"token_type\":1,\"weixin_cover_image\":{\"height\":1229,\"uri\":\"large/tos-cn-i-0000/0f9d6d0c-1df9-11e9-9893-0cc47a930ee4\",\"url\":\"http://p3-tt.bytecdn.cn/large/tos-cn-i-0000/0f9d6d0c-1df9-11e9-9893-0cc47a930ee4\",\"url_list\":[{\"url\":\"http://p3-tt.bytecdn.cn/large/tos-cn-i-0000/0f9d6d0c-1df9-11e9-9893-0cc47a930ee4\"},{\"url\":\"http://p9-tt.bytecdn.cn/large/tos-cn-i-0000/0f9d6d0c-1df9-11e9-9893-0cc47a930ee4\"},{\"url\":\"http://p3-tt.bytecdn.cn/large/tos-cn-i-0000/0f9d6d0c-1df9-11e9-9893-0cc47a930ee4\"}],\"width\":1023}},\"share_url\":\"http://m.toutiao.com/a6649155791372698119/?iid=0\\u0026app=news_article\\u0026is_hit_share_recommend=0\",\"show_dislike\":true,\"show_portrait\":false,\"show_portrait_article\":false,\"source\":\"家庭医生在线网\",\"source_icon_style\":4,\"source_open_url\":\"sslocal://profile?uid=53324103362\",\"tag\":\"news_health\",\"tag_id\":6649155791372698119,\"tip\":0,\"title\":\"晨起有这3个表现，是肝脏在向你报告：我生病了\",\"ugc_recommend\":{\"activity\":\"\",\"reason\":\"青云计划获奖者 优质健康领域创作者\"},\"url\":\"http://toutiao.com/group/6649155791372698119/\",\"user_info\":{\"avatar_url\":\"http://p1.pstatp.com/thumb/123200176d3c39c0cfda\",\"description\":\"由院士及国医大师指导，专注生产健康内容，做千家万户的家庭医生\",\"follow\":false,\"follower_count\":0,\"live_info_type\":1,\"name\":\"家庭医生在线网\",\"schema\":\"sslocal://profile?uid=53324103362\\u0026refer=all\",\"user_auth_info\":\"{\\\"auth_type\\\":\\\"0\\\",\\\"auth_info\\\":\\\"青云计划获奖者 优质健康领域创作者\\\",\\\"other_auth\\\":{\\\"interest\\\":\\\"优质健康领域创作者\\\"}}\",\"user_id\":53324103362,\"user_verified\":true,\"verified_content\":\"青云计划获奖者 优质健康领域创作者\"},\"user_repin\":0,\"user_verified\":1,\"verified_content\":\"青云计划获奖者 优质健康领域创作者\",\"video_style\":0}"
+
+    content = json.loads(data)
+
+    print(len(content['image_list']))
+
+    imgsrc = ''
+    for img in content['image_list']:
+        imgsrc = imgsrc + img['url'] + ','
+
+    print(imgsrc)
+
+def htmlParse():
+    options = Options()
+    options.add_argument('-headless')
+    driver = webdriver.Firefox(executable_path='D:\geckodriver\geckodriver.exe', options=options)
+    driver.get("http://toutiao.com/group/6630691620238918152/")
+    source = driver.page_source
+
+    soup = BeautifulSoup(source, "html.parser")
+    divs = soup.find_all(class_='article-content')[0]
+    print(divs)
+
+if __name__ == '__main__':
+    htmlParse()
+    # jsonParse()
--- toutiao/toutiao_news2.py
+++ toutiao/toutiao_news2.py
@@ -4,11 +4,8 @@
 import requests
 import re
 import json
-import math
-import random
 import time
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-import pandas as pd
 import pymysql
 import traceback
 from bs4 import BeautifulSoup
@@ -27,14 +24,10 @@ def ttapi(url):  ####APP模式
         'Accept-Language': 'zh-cn'
     }
     s.headers.update(headers)
-    df = pd.DataFrame(columns=(
-        'abstract 简报', 'title 标题', 'keywords 关键词', 'read_count 阅读量', 'share_count 分享数量',
-        'ban_comment 可评论', 'publish_time 推送时间', 'share_url url 链接', 'user_info_name 用户名',
-        'user_id 用户 id', 'description 用户描述', 'user_verified 官方账号', 'time 抓取时间', 'category 频道'
-    ))
+
     t2 = int(time.time()) - 500
-    x = 0
-    for i in range(10):  ###爬取页数
+
+    for i in range(20):  ###爬取页数
         time.sleep(3)
         t = int(time.time())
         params = {
@@ -57,81 +50,91 @@ def ttapi(url):  ####APP模式
         }
         url = 'http://is.snssdk.com/api/news/feed/v51/'
         app = s.get(url=url, params=params, verify=False).json()
-        # print(app)
+        print(app)
+
         t2 = t - 10
         total_number = app['total_number']
-        # print(total_number)
+
         for j in range(0, total_number):
             content = json.loads(app['data'][j]['content'])
+
             try:
                 abstract = content['abstract']  ##简报
+
             except:
                 abstract = ''
             try:
                 title = content['title']  ##标题
+                # result['title'] = title
             except:
                 title = ''
+
             try:
-                keywords = content['keywords']  ##关键词
+                comment_count = content['comment_count']  ##评论数
+                # result['commentCount'] = comment_count
             except:
-                keywords = ''
+                comment_count = ''
+
             try:
-                read_count = content['read_count']  ##阅读量
-            except:
-                read_count = ''
-            try:
-                share_count = content['share_count']  ##分享数量
-            except:
-                share_count = ''
-            try:
-                ban_comment = content['ban_comment']  ###是否可以评论，0为可评论，1不可评论
-            except:
-                ban_comment = ''
+                images = content['image_list']
+                imgsrc = ''
+                for img in images:
+                    imgsrc = imgsrc + img['url'] + ','
+                content['imgsrc'] = imgsrc
+            except KeyError:
+                content['imgsrc'] = ''
+
             try:
                 publish_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(content['publish_time']))  ##推送时间
+                # result['news_time'] = publish_time
             except:
                 publish_time = ''
+
             try:
-                share_url = content['share_url']  ###分享 url 链接
+                source = content['source']  ###新闻来源
+                # result['source'] = source
             except:
-                share_url = ''
+                source = ''
             try:
-                user_info_name = content['user_info']['name']  ##用户名
+                user_info_name = content['user_info']['name']  ##作者名称
+                # result['author'] = user_info_name
             except:
                 user_info_name = ''
-            try:
-                user_id = content['user_info']['user_id']  ##用户 id
-            except:
-                user_id = ''
-            try:
-                description = content['user_info']['description']  ##用户描述
-            except:
-                description = ''
-            try:
-                user_verified = content['user_info']['user_verified']  ###是否官方账号
-            except:
-                user_verified = ''
 
             try:
-                display_url = content['display_url']
+                url = content['display_url']
 
-                # options = webdriver.FirefoxOptions()
-                options = Options()
-                options.add_argument('-headless')
-                driver = webdriver.Firefox(options=options)
-                driver.get(display_url)
-
-                print(driver.page_source)
+                # options = Options()
+                # options.add_argument('-headless')
+                # driver = webdriver.Firefox(executable_path='D:\geckodriver\geckodriver.exe', options=options)
+                # driver.get(url)
+                # source = driver.page_source
+                #
+                # soup = BeautifulSoup(source, "html.parser")
             except:
-                display_url=''
+                content['content'] = ''
 
-            nowtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            df.loc[x] = [abstract, title, keywords, read_count, share_count, ban_comment,
-                         publish_time, share_url, user_info_name, user_id, description,
-                         user_verified, nowtime, channel]
-            x = x + 1
+            try:
+                tag = content['tag']
+            except:
+                tag = ''
 
-    df.to_csv('tt.csv', index=False, encoding="GB18030")
+            try:
+                news_id = content['tag_id']
+            except:
+                news_id = ''
+
+            content['channel'] = channel
+            # print(content['display_url'])
+            # print(content['content'])
+            # print(content['imgsrc'])
+            print(content['tag'])
+            print(content['tag_id'])
+            print(content['user_info']['name'])
+
+            # if content['content'] != '':
+            #     on_result(content)
+
     s.close()
 
 
@@ -165,6 +168,6 @@ def on_result(result=None):
         conn.close()
 
 if __name__ == '__main__':
-    # url = 'https://www.toutiao.com/ch/news_tech/'
-    url = 'https://www.toutiao.com/ch/selected/'
+    url = 'https://www.toutiao.com/ch/news_entertainment/'
+    # url = 'https://www.toutiao.com/ch/selected/'
     ttapi(url)
